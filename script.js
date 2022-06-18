c73e4d829c61df0289489bd749eede16d19d08d5
mmparadinha <matheusmparadinha@gmail.com>
2022-06-18 19:09:45 -0300

    Cartas desvirando, contador feito, encerramento OK. Pendência travar os apressados (pra não bugar), bônus e finalizar CSS dos controladores! AMÉM

--- script.js
+++ script.js
@@ -1,88 +1,100 @@
-let combinacoes = Number(prompt('Olá! Com quantas combinações deseja jogar? (2-7)'));
+let combinacoes = Number(prompt('Olá! Com quantas combinações deseja jogar? (2 à 7)'));
 const cartas = [];
 const ul = document.querySelector("ul");
 let parrot1;
 let parrot2;
+let card1;
+let card2;
+let jogadas = 0;
+let acertos = 0;
+let tempo = 0;
 
 while (combinacoes < 2 || combinacoes > 7) {
-    combinacoes = Number(prompt('Não podemos iniciar o jogo com esse número de combinações. Escolha novamente! (2-7)'));
+    combinacoes = Number(prompt('Não podemos iniciar o jogo com esse número de combinações. Escolha novamente! (2 à 7)'));
 }
+
+function contador() {
+    tempo++;
+    document.querySelector(".contador").innerHTML = tempo;
+}
+
+let idInterval = setInterval(contador, 1000);
 
 function tirarCartaBobRoss() {
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/bobrossparrot.gif" alt="bobrossparrot">
-   `);
+<img src="images/front.png" alt="parrot">
+<img src="images/bobrossparrot.gif" alt="bobrossparrot">
+`);
     cartas.push(`
-        <img src="images/front.png" alt="parrot">
-        <img src="images/bobrossparrot.gif" alt="bobrossparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/bobrossparrot.gif" alt="bobrossparrot">
+`);
 }
 
 function tirarCartaExplody() {
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/explodyparrot.gif" alt="explodyparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/explodyparrot.gif" alt="explodyparrot">
+`);
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/explodyparrot.gif" alt="explodyparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/explodyparrot.gif" alt="explodyparrot">
+`);
 }
 
 function tirarCartaFiesta() {
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Fiestaparrot.gif" alt="Fiestaparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Fiestaparrot.gif" alt="Fiestaparrot">
+`);
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Fiestaparrot.gif" alt="Fiestaparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Fiestaparrot.gif" alt="Fiestaparrot">
+`);
 }
 
 function tirarCartaMetal() {
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Metalparrot.gif" alt="Metalparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Metalparrot.gif" alt="Metalparrot">
+`);
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Metalparrot.gif" alt="Metalparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Metalparrot.gif" alt="Metalparrot">
+`);
 }
 
 function tirarCartaRevertit() {
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Revertitparrot.gif" alt="Revertitparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Revertitparrot.gif" alt="Revertitparrot">
+`);
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Revertitparrot.gif" alt="Revertitparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Revertitparrot.gif" alt="Revertitparrot">
+`);
 }
 
 function tirarCartaTriplets() {
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Tripletsparrot.gif" alt="Tripletsparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Tripletsparrot.gif" alt="Tripletsparrot">
+`);
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Tripletsparrot.gif" alt="Tripletsparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Tripletsparrot.gif" alt="Tripletsparrot">
+`);
 }
 
 function tirarCartaUnicorn() {
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Unicornparrot.gif" alt="Unicornparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Unicornparrot.gif" alt="Unicornparrot">
+`);
     cartas.push(`
-    <img src="images/front.png" alt="parrot">
-    <img src="images/Unicornparrot.gif" alt="Unicornparrot">
-    `);
+<img src="images/front.png" alt="parrot">
+<img src="images/Unicornparrot.gif" alt="Unicornparrot">
+`);
 }
 
 //troço horroroso, mas eu tava travado tentando fazer bonito
@@ -129,16 +141,30 @@ cartas.sort(comparador);
 
 for (const element of cartas) {
     ul.innerHTML += `
-      <li class="card front-face" onclick="virarCarta(this)">${element}</li>
+      <li onclick="virarCarta(this)">${element}</li>
     `;
 }
 
+function limpaVariáveis() {
+    parrot1 = undefined;
+    parrot2 = undefined;
+    card1 = undefined;
+    card2 = undefined;
+}
+
 function virarCarta(element) {
-    element.classList.toggle('front-face');
-    element.classList.toggle('back-face');
+    jogadas++;
+    document.querySelector(".jogadas").innerHTML = jogadas;
+    element.classList.add('back');
     let p = 1;
 
-    if (parrot1 == undefined) {
+    if (card1 === undefined) {
+        card1 = element;
+    } else {
+        card2 = element;
+    }
+
+    if (parrot1 === undefined) {
         parrot1 = element.innerHTML;
     } else {
         parrot2 = element.innerHTML;
@@ -146,21 +172,21 @@ function virarCarta(element) {
     }
 
     if (p === 2) {
-        if (parrot1 != parrot2) {
-            alert("errôô");
-            //virando só a última carta
-            //element.classList.toggle('front-face');
-            //element.classList.toggle('back-face');
+        if (parrot1 !== parrot2) {
+            setTimeout(() => card1.classList.remove('back'), 1000);
+            setTimeout(() => card2.classList.remove('back'), 1000);
         } else {
-            alert("acertô mizeravi");
+            acertos++;
+            document.querySelector(".acertos").innerHTML = acertos;
         }
-        parrot1 = undefined;
-        parrot2 = undefined;
+        setTimeout(limpaVariáveis, 1100);
+    }
+
+    if (acertos === combinacoes) {
+        clearInterval(idInterval);
+        alert(`Você ganhou em ${tempo} segundos com ${jogadas} jogadas!`);
     }
 }
-
-
-
 
 
 
